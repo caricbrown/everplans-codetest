0288d16ee3d2372c03b5f4a5ffd02d74df8a5ffa
Aric <35151612+capacitivellc@users.noreply.github.com>
2021-05-15 11:33:02 -0700

    Initial Commit

---
+++ CodeTest/CodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTestApp.swift
+//  CodeTest
+//
+//  Created by Aric Brown on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
